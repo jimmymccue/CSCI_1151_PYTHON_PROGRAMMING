2e8e44ce11ac93a870b4228ab483c856a975f4f0
Jimmy McCue <xstreamfishn@gmail.com>
2025-07-04 10:17:33 -0400

    configured folder structure to test properly

---
+++ unit_10/src/rotation_function.py
@@ -0,0 +1,3 @@
+
+def strip_rotation():
+  pass
---
+++ unit_10/test/test_rotation_function.py
@@ -0,0 +1,28 @@
+import sys
+import os
+
+# Add the ../src directory to the import path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
+from rotation_function import strip_rotation
+
+def test_100_returns_100():
+    pass
+
+# def test_460_returns_100():
+#     pass
+
+# def test_820_returns_100():
+#     pass
+
+# def test_negative_100_returns_260():
+#     pass
+
+# def test_negative_460_returns_260():
+#     pass
+
+# def test_negative_820_returns_260():
+#     pass
+
+# def test_non_numeric_catches_exception():
+#     pass
